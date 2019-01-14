f6c53749b2f38f6d0de4f8baa95646d8dd9b32cf
Randy Michael Churchill <1577108+rmchurch@users.noreply.github.com>
2019-01-14 16:38:39 -0500

    Distributed training, with multi-process, single GPU in place (though may need additional Python 3 changes)

--- main.py
+++ main.py
@@ -31,32 +31,27 @@ parser.add_argument('--optim', type=str, default='Adam',
                     help='optimizer to use (default: Adam)')
 parser.add_argument('--nhid', type=int, default=25,
                     help='number of hidden units per layer (default: 25)')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed (default: 1111)')
+parser.add_argument('--seed', type=int, default=None,
+                    help='random seed (default: None)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--world-size', default=-1, type=int,
                     help='number of nodes for distributed training')
 parser.add_argument('--rank', default=-1, type=int,
                     help='node rank for distributed training')
-parser.add_argument('--dist-backend', default='nccl', type=str,
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='gloo', type=str,
                     help='distributed backend')
 parser.add_argument('--multiprocessing-distributed', action='store_true',
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
                          'fastest way to use PyTorch for either single node or '
                          'multi node data parallel training')
-args = parser.parse_args()
 
-torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-args.distributed = args.world_size > 1 or args.multiprocessing_distributed
 
-if not args.distributed: args.workers = 0
-
+#TODO Generalize
 root = '/scratch/gpfs/rmc2/ecei_d3d/'
 data_root = root+'data/'
 clear_file = root + 'd3d_clear_ecei.final.txt'
@@ -64,66 +59,147 @@ disrupt_file = root + 'd3d_disrupt_ecei.final.txt'
 batch_size = 1#args.batch_size
 n_classes = 1 #for binary classification
 input_channels = 160
-epochs = args.epochs
 steps = 0
 #seq_length = int(Nseq) #TODO: This might be different from how I defined it in loader.py
-#TODO remove, use general passed in instead
-args.nhid = 20
-args.levels = 5
 dilation_sizes = [1,10,100,1000,6783] #dilation=10, except last which is set to give receptive field ~Nmodel=300,000
 Nsub = 1000000 #found by taking receptive field, and scaling for 15GB of GPU memory #TODO automate
 Nrecept = 300000
-args.log_interval = 1
-#TODO have to write overlapping sequences, not just put in
 
-print(args)
-dataset = EceiDataset(data_root,clear_file,disrupt_file)
-#create the indices for train/val/test split
-dataset.train_val_test_split()
-#create data loaders
-train_loader, val_loader, test_loader = data_generator(dataset, batch_size, 
-                                                        distributed=args.distributed,
-                                                        num_workers=args.workers)
 
-channel_sizes = [args.nhid] * args.levels
-kernel_size = args.ksize
-model = TCN(input_channels, n_classes, channel_sizes, 
-            kernel_size=kernel_size, 
-            dropout=args.dropout,
-            dilation_size=dilation_sizes)
 
-if args.cuda:
-    model.cuda()
+def main():
+    args = parser.parse_args()
 
-def process_seq(x,target,Nsub):
-    '''Splits apart sequence into equal, overlapping subsequences of length Nsub, with overlap Nrecept
-    Does accumulated gradients method to avoid large GPU memory usage
-    '''
-    N = x.shape[-1] #length of entire sequence
-    num_seq_frac = (N - Nsub)/float(Nsub - Nrecept + 1)+1
-    num_seq = np.ceil(num_seq_frac).astype(int)
-    total_losses = 0
-    for m in range(num_seq):
-        start_idx =    m*Nsub - m*Nrecept + m
-        stop_idx = (m+1)*Nsub - m*Nrecept + m
-        if stop_idx>N: stop_idx = N
-        if ((stop_idx-start_idx)<Nrecept):
-            start_idx = stop_idx - Nrecept
-        ys = model(x[...,start_idx:stop_idx])
-        ts = target[...,start_idx:stop_idx]
-        #do mean of loss by hand to handle unequal sequence lengths
-        loss = F.binary_cross_entropy(ys[...,Nrecept-1:],ts[...,Nrecept-1:],reduction='sum')/(N-Nrecept+1)
-        loss.backward()
-        if args.clip > 0:
-            torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
-        total_losses += loss.item()
-    return total_losses
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
 
-lr = args.lr
-#optimizer = getattr(optim, args.optim)(model.parameters(), lr=lr)
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, nesterov=True)
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-def train(epoch):
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+
+    ngpus_per_node = torch.cuda.device_count()
+
+    if args.multiprocessing_distributed:
+        # Since we have ngpus_per_node processes per node, the total world_size
+        # needs to be adjusted accordingly
+        args.world_size = ngpus_per_node * args.world_size
+        # Use torch.multiprocessing.spawn to launch distributed processes: the
+        # main_worker process function
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    else:
+        # Simply call main_worker function
+        main_worker(args.gpu, ngpus_per_node, args)
+
+def main_worker(gpu,ngpus_per_node,args):
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    if args.distributed:
+        if args.dist_url == "env://" and args.rank == -1:
+            args.rank = int(os.environ["RANK"])
+        if args.multiprocessing_distributed:
+            # For multiprocessing distributed training, rank needs to be the
+            # global rank among all the processes
+            args.rank = args.rank * ngpus_per_node + gpu
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size, rank=args.rank)
+
+    #TODO remove, use general passed in instead
+    epochs = args.epochs
+    args.nhid = 20
+    args.levels = 5
+    args.log_interval = 1
+    #TODO have to write overlapping sequences, not just put in
+
+    channel_sizes = [args.nhid] * args.levels
+    kernel_size = args.ksize
+    model = TCN(input_channels, n_classes, channel_sizes, 
+                kernel_size=kernel_size, 
+                dropout=args.dropout,
+                dilation_size=dilation_sizes)
+
+    if args.distributed:
+        # For multiprocessing distributed, DistributedDataParallel constructor
+        # should always set the single device scope, otherwise,
+        # DistributedDataParallel will use all available devices.
+        if args.gpu is not None:
+            torch.cuda.set_device(args.gpu)
+            model.cuda(args.gpu)
+            # When using a single GPU per process and per
+            # DistributedDataParallel, we need to divide the batch size
+            # ourselves based on the total number of GPUs we have
+            args.batch_size = int(args.batch_size / ngpus_per_node)
+            args.workers = int(args.workers / ngpus_per_node)
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        else:
+            model.cuda()
+            # DistributedDataParallel will divide and allocate batch_size to all
+            # available GPUs if device_ids are not set
+            model = torch.nn.parallel.DistributedDataParallel(model)
+    elif args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+
+    print(args)
+    dataset = EceiDataset(data_root,clear_file,disrupt_file)
+    #create the indices for train/val/test split
+    dataset.train_val_test_split()
+    #create data loaders
+    train_loader, val_loader, test_loader = data_generator(dataset, batch_size, 
+                                                            distributed=args.distributed,
+                                                            num_workers=args.workers)
+
+    lr = args.lr
+    #optimizer = getattr(optim, args.optim)(model.parameters(), lr=lr)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, nesterov=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+    for epoch in range(args.start_epoch, args.epochs):
+        #TODO: Determine if I want this LR decrease, or automated ways (momentum?)
+        adjust_learning_rate(optimizer, epoch, args)
+
+        # train for one epoch
+        train(train_loader, model, optimizer, epoch, args)
+
+        # evaluate on validation set
+        acc1 = evaluate(val_loader, model, criterion, args)
+
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
+                and args.rank % ngpus_per_node == 0):
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_acc1': best_acc1,
+                'optimizer' : optimizer.state_dict(),
+            }, is_best)
+
+def train(train_loader, model, optimizer, epoch, args):
     global steps #TODO: why do I need steps??
     train_loss = 0
     model.train()
@@ -168,14 +244,39 @@ def evaluate(epoch):
         return loss
 
 
+def process_seq(x,target,Nsub):
+    '''Splits apart sequence into equal, overlapping subsequences of length Nsub, with overlap Nrecept
+    Does accumulated gradients method to avoid large GPU memory usage
+    '''
+    N = x.shape[-1] #length of entire sequence
+    num_seq_frac = (N - Nsub)/float(Nsub - Nrecept + 1)+1
+    num_seq = np.ceil(num_seq_frac).astype(int)
+    total_losses = 0
+    for m in range(num_seq):
+        start_idx =    m*Nsub - m*Nrecept + m
+        stop_idx = (m+1)*Nsub - m*Nrecept + m
+        if stop_idx>N: stop_idx = N
+        if ((stop_idx-start_idx)<Nrecept):
+            start_idx = stop_idx - Nrecept
+        ys = model(x[...,start_idx:stop_idx])
+        ts = target[...,start_idx:stop_idx]
+        #do mean of loss by hand to handle unequal sequence lengths
+        loss = F.binary_cross_entropy(ys[...,Nrecept-1:],ts[...,Nrecept-1:],reduction='sum')/(N-Nrecept+1)
+        loss.backward()
+        if args.clip > 0:
+            torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+        total_losses += loss.item()
+    return total_losses
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
 if __name__ == "__main__":
     tstart = time.time()
-    for epoch in range(1, epochs+1):
-        train(epoch)
-        evaluate(epoch)
-        #TODO: Determine if I want this LR decrease, or automated ways (momentum?)
-        if epoch % 10 == 0:
-            lr /= 10
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
+    main()
     #TODO: test data set, create final statistics (ROC? Printed recall/precision?)
