Fix --seed. Add --test-indices to specify global indices of dataset. Add --weight-balance to allow no weighting in cross-entropy loss

loader.py
@@ -12,7 +12,8 @@ class EceiDataset(data.Dataset):
     def __init__(self,root,clear_file,disrupt_file,
                       train=True,flattop_only=True,
                       Twarn=300,
-                      test=0):
+                      test=0,test_indices=None,
+                      weight_balance=True):
         """Initialize
         root: Directory root. Must have 'disrupt/' and 'clear/' as subdirectories
         clear_file, disrupt_file: File paths for disrupt/clear ECEi datasets.
@@ -29,7 +30,9 @@ class EceiDataset(data.Dataset):
         train: 
         flattop_only: Use only flattop times
         Twarn: time before disruption we wish to begin labelling as "disruptive"
-        test: number of recurring data points to give
+        test: training set size (# shots), for testing of overfitting purposes
+        test_indices: List of specific global indices (len(test_indices) must match test)
+        weight_balance: For imbalanced label sets, uses weight to balance in binary cross entropy  (default True)
         """
         self.root = root
         self.train = train #training set or test set TODO: Do I need this?
@@ -89,9 +92,13 @@ class EceiDataset(data.Dataset):
                 disinds = np.where(self.disrupted)[0]
                 self.test_indices = disinds[np.random.randint(disinds.size,size=1)]
             else:
-                _,self.test_indices,_,_ = train_test_split(np.arange(self.shot.size),labels,
+                if test_indices is None:
+                    _,self.test_indices,_,_ = train_test_split(np.arange(self.shot.size),labels,
                                                        stratify=labels,
                                                        test_size=self.test)
+                else:
+                    assert len(test_indices)==self.test
+                    self.test_indices = np.array(test_indices)
             for ind in self.test_indices:
                 self.test_data += [self.read_data(ind)]
 
@@ -100,11 +107,14 @@ class EceiDataset(data.Dataset):
         #for now, do a constant weight on the disrupted class, to balance the unbalanced set
         #TODO implement increasing weight towards final disruption
         if inds is None: inds = np.arange(len(self.shot))
-        N = np.sum(self.stop_idx[inds] - self.start_idx[inds])
-        disinds = inds[self.disrupted[inds]]
-        Ndisrupt = np.sum(self.stop_idx[disinds] - self.disrupt_idx[disinds])
-        Nnondisrupt = N - Ndisrupt
-        self.weight = Nnondisrupt/Ndisrupt
+        if self.weight_balance:
+            N = np.sum(self.stop_idx[inds] - self.start_idx[inds])
+            disinds = inds[self.disrupted[inds]]
+            Ndisrupt = np.sum(self.stop_idx[disinds] - self.disrupt_idx[disinds])
+            Nnondisrupt = N - Ndisrupt
+            self.weight = Nnondisrupt/Ndisrupt
+        else:
+            self.weight = 1
 
     def train_val_test_split(self,sizes=[0.8,0.1,0.1]):
         """Creates indices to split data into train, validation, and test datasets. 

main.py
@@ -1,6 +1,9 @@
 #!/usr/bin python
+import random
+import warnings
 import torch
 import torch.distributed as dist
+import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 import torch.optim as optim
 import torch.nn.functional as F
@@ -55,6 +58,8 @@ parser.add_argument('--iterations-valid', type=int, default=200,
                     help='iteration period to run validation(default: 200)')
 parser.add_argument('--optim', type=str, default='SGD',
                     help='optimizer to use (default: SGD)')
+parser.add_argument('--weight-balance', action='store_false',
+                    help='Balance an imbalanced dataset with weight in cross-entropy loss (default: True)')
 #other
 parser.add_argument('--cuda', action='store_false',
                     help='use CUDA (default: True)')
@@ -81,6 +86,8 @@ parser.add_argument('--multiprocessing-distributed', action='store_true',
                          'multi node data parallel training')
 parser.add_argument('--test', default=0, type=int, metavar='N',
                     help='runs on single example, to verify model can overfit (default: 0)')
+parser.add_argument('--test-indices', default=None, nargs='*',type=int,
+                    help='list of global indices to use (default: None)')
 
 
 
@@ -194,7 +201,9 @@ def main_worker(gpu,ngpus_per_node,args):
         # model = torch.nn.DataParallel(model).cuda()
 
     print(args)
-    dataset = EceiDataset(data_root,clear_file,disrupt_file,test=args.test)
+    dataset = EceiDataset(data_root,clear_file,disrupt_file,
+                          test=args.test,test_indices=args.test_indices,
+                          weight_balance=args.weight_balance)
     #create the indices for train/val/test split
     dataset.train_val_test_split()
     #create data loaders
@@ -264,7 +273,9 @@ def main_worker(gpu,ngpus_per_node,args):
                 print('Train Epoch: %d [%d/%d (%0.2f%%)]\tIndex: %d\tDisrupted: %d\tLoss: %0.6e\tSteps: %d\tTime: %0.2f\tMem: %0.1f\tLR: %0.2e' % (
                             epoch, (batch_idx*args.world_size+args.rank), len(train_loader.dataset),
                             100. * (batch_idx*args.world_size+args.rank)  / len(train_loader.dataset), global_index, train_loader.dataset.dataset.disrupted[global_index],train_loss, steps,(time.time()-args.tstart),psutil.virtual_memory().used/1024**3.,lr_epoch))
-
+        
+        print('Train Epoch: %d \tTotal Loss: %0.6e\tSteps: %d\tTime: %0.2f\tLR: %0.2e' % (
+                            epoch, total_loss, steps,(time.time()-args.tstart),lr_epoch))
     if (args.test>0):
         plt.figure(figsize=[6.40,7.40])
         for batch_idx, (data, target, global_index, weight) in enumerate(train_loader):