6b5c66268f00e744d64393fb3ba15937bd9ceb7b
Randy M. Churchill <rmc2@tigergpu.princeton.edu>
2019-04-05 18:35:11 -0400

    Fixed bug where doing all_reduce, but only calling on rank==0

--- main.py
+++ main.py
@@ -329,12 +329,13 @@ def main_worker(gpu,ngpus_per_node,args):
             total_loss += train_loss
 
             #log training 
-            if (batch_idx % args.log_interval == 0) and (args.rank==0):
+            if batch_idx % args.log_interval == 0:
                 total_loss = all_reduce(total_loss).item()
-                lr_epoch = [ group['lr'] for group in optimizer.param_groups ][0]
-                print('Train Epoch: %d [%d/%d (%0.2f%%)]\tIteration: %d\tDisrupted: %0.4f\tLoss: %0.6e\tSteps: %d\tTime: %0.2f\tMem: %0.1f\tLR: %0.2e' % (
-                            epoch, batch_idx, len(train_loader), 100. * (batch_idx / len(train_loader)), iteration,
-                            np.sum(train_loader.dataset.dataset.disruptedi[global_index])/global_index.size(), total_loss/args.log_interval, steps,(time.time()-args.tstart),psutil.virtual_memory().used/1024**3.,lr_epoch))
+                if args.rank==0:
+                    lr_epoch = [ group['lr'] for group in optimizer.param_groups ][0]
+                    print('Train Epoch: %d [%d/%d (%0.2f%%)]\tIteration: %d\tDisrupted: %0.4f\tLoss: %0.6e\tSteps: %d\tTime: %0.2f\tMem: %0.1f\tLR: %0.2e' % (
+                                epoch, batch_idx, len(train_loader), 100. * (batch_idx / len(train_loader)), iteration,
+                                np.sum(train_loader.dataset.dataset.disruptedi[global_index])/global_index.size(), total_loss/args.log_interval, steps,(time.time()-args.tstart),psutil.virtual_memory().used/1024**3.,lr_epoch))
                 total_loss = 0
     
             #validate
