2c047357d4ca2a2b8441465091167dc0b2751464
Michael Churchill <rmc256@gmail.com>
2020-08-28 10:39:44 -0400

    Modified run.sh for nsys

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #BSUB -P gen141
 #BSUB -J test_disrupt
-#BSUB -W 00:30 #30 minutes
+#BSUB -W 01:00 #30 minutes
 ##BSUB -q debug
 #BSUB -nnodes 1
+#BSUB -alloc_flags maximizegpfs
 
 #echo this run.sh file contents in the log file, to capture inputs
 echo "$0"
@@ -30,19 +31,27 @@ git --git-dir=$PWD/disruptcnn/.git  show --oneline -s
 #for cProfile, to force sync CUDA ops
 #export CUDA_LAUNCH_BLOCKING=0
 
-export OMP_NUM_THREADS=6
+export OMP_NUM_THREADS=8
 
 file="file:///gpfs/wolf/gen141/scratch/rchurchi/main_${LSB_JOBID}.txt"
 #fileprof="profile_${LSB_JOBID}_rank_${LS_JOBPID}.prof"
 #source activate torch-env
-jsrun --nrs 6 --rs_per_host 6 --tasks_per_rs 1 --cpu_per_rs 6 --gpu_per_rs 1 \
-		   python -u -m torch.utils.bottleneck disruptcnn/main.py --dist-url $file --backend 'nccl' \
-                    --batch-size=12 --dropout=0.1 --clip=0.3 \
+module load ibm-wml-ce
+conda activate /gpfs/wolf/gen141/proj-shared/disruptCNN/conda-envs/torch-env
+module load nsight-systems
+jsrun --nrs 6 --rs_per_host 6 --tasks_per_rs 1 --cpu_per_rs 6 -b packed:6 --gpu_per_rs 1 \
+                   nsys profile -t cuda,nvtx -s cpu -o profile_%q{OMPI_COMM_WORLD_RANK} \
+		   /gpfs/wolf/gen141/proj-shared/disruptCNN/conda-envs/torch-env/bin/python -u disruptcnn/main.py --dist-url $file --backend 'nccl' \
+                    --batch-size=2 --dropout=0.1 --clip=0.3 \
                     --lr=0.5 \
                     --workers=6 \
                     --nsub 78125 \
-                    --epochs=2 \
+                    --epochs=4 \
                     --label-balance='const' \
                     --data-step=10 --levels=4 --nrecept=30000 --nhid=80 \
                     --undersample \
-                    --iterations-valid 600
+                    --iterations-valid 600 \
+        		    --test-indices 0 1 2 3 4 5
+
+#try to ensure the profiler and file movement finishes with everything
+sleep 60
