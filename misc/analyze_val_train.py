8bd9daf5dc56bae8b9c11c7a5c72ac3ab26f5952
rmchurch <rmc256@gmail.com>
2020-10-02 17:31:41 -0400

    Plotting changes for TPR

--- misc/analyze_val_train.py
+++ misc/analyze_val_train.py
@@ -12,7 +12,7 @@ import sys
 
 def read_losses(filename):
     epochs = []; losses = []; iterations = []; steps = []; times = []; lrs = []
-    val_index = []; val_losses = []; val_acc = []; val_f1 = []; val_times = []; thresholds = []
+    val_index = []; val_losses = []; val_acc = []; val_tpr = []; val_fpr = [];  val_f1 = []; val_times = []; thresholds = []
     lr = 0
     with open(filename) as f:
         for line in f:
@@ -34,6 +34,12 @@ def read_losses(filename):
                     val_acc += [float(line.split('Accuracy:')[1].split()[0])]
                 val_index += [len(epochs)-1]
                 val_losses += [float(line.split('Average loss:')[1].split()[0])]
+                if 'TPR' in line: #TPR introduced later, this for backwards compatability
+                    val_tpr += [float(line.split('TPR:')[1].split()[0])]
+                    val_fpr += [float(line.split('FPR:')[1].split()[0])]
+                else:
+                    val_tpr += [0]
+                    val_fpr += [0]
                 val_f1 += [float(line.split('F1:')[1].split()[0])]
                 if 'Threshold' in line:
                     thresholds += [float(line.split('Threshold:')[1].split()[0])]
@@ -44,7 +50,7 @@ def read_losses(filename):
     val_iterations = np.array([iterations[i] for i in val_index])
     val_losses = np.array(val_losses); val_acc = np.array(val_acc); val_f1 = np.array(val_f1)
     return lr,epochs,losses, iterations, steps, times, lrs, \
-           val_epochs, val_iterations, val_losses, val_acc, val_f1, thresholds
+           val_epochs, val_iterations, val_losses, val_acc, val_tpr, val_fpr, val_f1, thresholds
 
 if __name__=="__main__":
     files = glob.glob('slurm*out')
@@ -62,15 +68,15 @@ if __name__=="__main__":
 
     for i,f in enumerate(files):
         #if '615' in f: continue
-        lr,epochs,losses, iterations, steps, times, lrs, val_epochs, val_iterations, val_losses, val_acc, val_f1, thresholds = read_losses(f)
+        lr,epochs,losses, iterations, steps, times, lrs, val_epochs, val_iterations, val_losses, val_acc, val_tpr, val_fpr, val_f1, thresholds = read_losses(f)
         if i==0:
             plt.figure(1,figsize=[13,4.5]); plt.clf()
             ax1 = plt.subplot(131)
             ax2 = plt.subplot(132,sharex=ax1)
             ax3 = plt.subplot(133,sharex=ax1)
             plt.tight_layout()
+        ax1.plot(val_epochs,val_losses,'-o',color=colors[i],alpha=0.25)
         ax1.plot(epochs,losses,label=str(lr)+','+f,color=colors[i])
-        ax1.plot(val_epochs,val_losses,'-o',color=colors[i])
         ax1.set_yscale('log')
         ax2.plot(val_epochs,val_acc,'-o',color=colors[i],)
         ax2.plot(val_epochs,val_f1,'-d',color=colors[i])
